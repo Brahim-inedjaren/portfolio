0a96482c2722c8ade8efd858ff2619ed40a4b4e7
Brahim-inedjaren <ibrahim10inedjaren@gmail.com>
2026-02-07 10:50:13 +0100

    Force rebuild for deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero: React.FC = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        Hi, I'm {content.hero.name}
+        Bonjour, je suis {content.hero.name}
       </motion.h1>
       <motion.p
         initial={{ opacity: 0, y: 20 }}
